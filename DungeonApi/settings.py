2838af25269b8908cdd8a126ca11caa6cb860000
Maxwell Baird <bairdm71@gmail.com>
2020-07-28 15:50:25 -0600

    remove password from database

--- DungeonApi/settings.py
+++ DungeonApi/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dungeoncrawler',
         'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1'
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
