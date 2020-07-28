79d4758c8c2a4ff315b3eebd9718aa555332d96f
Maxwell Baird <bairdm71@gmail.com>
2020-07-28 14:57:28 -0600

    added travis

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/maxwellbaird/turing/module4/dungeon-crawler/Dungeon-Crawler-BE/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
